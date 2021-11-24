655ab09a7f78a7a9f5999586f51964b35e7c5f5c
arma7x <arma7x@live.com>
2021-11-25 04:32:14 +0800

    update

--- app.js
+++ app.js
@@ -1163,7 +1163,7 @@ window.addEventListener("load", function() {
       return;
     getKaiAd({
       publisher: 'ac3140f7-08d6-46d9-aa6f-d861720fba66',
-      app: 'kfm',
+      app: 'the-economy',
       slot: 'kaios',
       onerror: err => console.error(err),
       onready: ad => {
